bc62187b017095318ed6ea17a62e8feaad5e33a5
SamLind11 <samuellind11@gmail.com>
2023-10-19 13:51:10 -0400

    added post functionality

--- static/js/games.js
+++ static/js/games.js
@@ -1,9 +1,8 @@
 // Name variables for html elements.
 const form = document.getElementById("form");
 const datePlayed = document.getElementById("datePlayed");
-const whiteId = document.getElementById("whiteID");
-const blackId = document.getElementById("blackID");
-const result = document.querySelector('input[name="result"]:checked');
+const whiteId = document.getElementById("whiteId");
+const blackId = document.getElementById("blackId");
 const pgn = document.getElementById("pgn");
 const submit = document.getElementById("submit");
 
@@ -13,29 +12,99 @@ form.addEventListener('submit', addGame);
 function addGame(e) {
     e.preventDefault();
 
-    let whiteScore = 1;
-    let blackScore = 0;
+    // Fetch user ratings from API.
+    let whiteOldRating;
+    let blackOldRating;
+    
+    fetch(`http://localhost:8000/members/${whiteId.value}`)
+        .then(response => response.json())
+        .then(data => {
+            whiteOldRating = data.rating;
+
+            fetch(`http://localhost:8000/members/${blackId.value}`)
+            .then(response => response.json())
+            .then(data => {
+                blackOldRating = data.rating;
+
+                // Calls function to post new game once API calls have succeeded.
+                postGame(whiteOldRating, blackOldRating);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    
+}
+
+// Function which posts new game to the database.
+function postGame(wRating, bRating) {
+    const result = document.querySelector('input[name="result"]:checked');
+
+    let whiteScore;
+    let blackScore;
+    let newRatings;
+    let whiteNewRating;
+    let blackNewRating;
     let gameResult = result.value;
-    if (gameResult === 'black') {
+    if (gameResult === 'white') {
+        whiteScore = 1;
+        blackScore = 0;
+        newRatings = ratingCalc(wRating, bRating);
+        whiteNewRating = newRatings[0];
+        blackNewRating = newRatings[1];
+    } else if (gameResult === 'black') {
         whiteScore = 0;
         blackScore = 1;
-    } else if (gameResult === 'draw') {
+        newRatings = ratingCalc(bRating, wRating);
+        whiteNewRating = newRatings[1];
+        blackNewRating = newRatings[0];
+    } else {
+        // Game is a draw.
         whiteScore = 0.5;
         blackScore = 0.5;
+        newRatings = ratingCalc(wRating, bRating, draw=true);
+        whiteNewRating = newRatings[0];
+        blackNewRating = newRatings[1];
     }
-
+    // New game object to be added.
     newGame = {
         datePlayed: datePlayed.value,
         whiteId: whiteId.value,
         blackId: blackId.value,
-        whiteScore: whiteScore, //TODO
-        blackScore: blackScore, //TODO
-        whiteOldRating: whiteOldRating, //TODO
-        whiteNewRating: whiteNewRating, //TODO
-        blackOldRating: blackOldRating, //TODO
-        blackNewRating: blackNewRating, //TODO
+        whiteScore: whiteScore, 
+        blackScore: blackScore, 
+        whiteOldRating: wRating, 
+        whiteNewRating: whiteNewRating, 
+        blackOldRating: bRating, 
+        blackNewRating: blackNewRating, 
         pgn: pgn.value,
     }
+
+    // Convert the data to JSON
+    let game = JSON.stringify(newGame);
+
+    // Send a POST request
+    fetch('http://localhost:8000/games/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: game,
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        datePlayed.value = '';
+        whiteId.value = '';
+        blackId.value = '';
+        pgn.value = '';
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
 
 // Calculate new ratings after a game.
@@ -68,3 +137,4 @@ function ratingCalc(winnerRating, loserRating, draw=false) {
     
     return [winnerNew, loserNew];
 }
+
